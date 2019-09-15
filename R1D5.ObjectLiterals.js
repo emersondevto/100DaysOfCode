176fb9ad73a54c5b1ce3bde23ba960ece2a4a04e
Emerson Volkov <emersonvolkov@gmail.com>
2019-09-15 16:11:35 -0500

    #100DaysOfCode R1D5 Object Literals en #Javascript son como las clases anónimas en otros lenguajes de programación como Java. Tener en cuenta que la notación aunque similar no es igual a JSON.

--- R1D5.ObjectLiterals.js
+++ R1D5.ObjectLiterals.js
@@ -1,1 +1,51 @@
 console.log("Objetos Literals");
+
+// Un Object Literal es un objeto con estructura tipo JSON y que en Javascript tiene un uso igual al que tiene un objeto en otros lenguajes de programacion.
+
+// Todo es manejado en un modelo de key value pares y dentro de un Object Literal
+
+const person = {
+  firstName: "Emerson",
+  lastName: "Volkov",
+  age: 31,
+  email: "example@example.com",
+  hobbies: ["music", "sports", "video games"],
+  address: {
+    city: "Bogota",
+    state: "Cundinamarca"
+  },
+  getBirthYear: function() {
+    return 2019 - this.age;
+  }
+
+  // name: this.firstName + this.lastName
+};
+
+console.log("print object person");
+console.table(person);
+
+console.log("get a specific value");
+console.log(person.firstName);
+console.log(person["firstName"]);
+
+console.log("get a specific insight other object");
+console.log(person.address.city);
+
+console.log("Ejecutando una funcion almacenada en un Object Literal");
+console.log(person.getBirthYear());
+
+console.log("Obteniendo un valor del array");
+console.log(person.hobbies[1]);
+
+console.log("Tambien es posible tener arrays de objects literals");
+const people = [
+  { name: "Emerson", age: 21 },
+  { name: "Alejandra", age: 31 },
+  { name: "Andres", age: 33 }
+];
+console.table(people);
+
+for (let i = 0; i < people.length; i++) {
+  const element = people[i];
+  console.log(element.name);
+}
