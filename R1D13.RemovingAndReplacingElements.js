4ee1f64edf524a4ebb89d1239247d75971b53b42
Emerson Volkov <emersonvolkov@gmail.com>
2019-09-23 07:25:06 -0500

    #100DaysOfCode R1D13 Remover y reemplazar elementos del DOM, remover y agregar classes de un nodo del DOM, y remover, agregar y modificar atributos en un elemento del DOM con #javascript.

--- R1D13.RemovingAndReplacingElements.js
+++ R1D13.RemovingAndReplacingElements.js
@@ -1,1 +1,70 @@
 console.log("Removing & Replacing Elements");
+
+// REPLACE ELEMENT
+
+//create element
+const newHeading = document.createElement("h2");
+
+// Add id
+const oldHeading = document.querySelector("#task-title");
+
+newHeading.id = oldHeading.id;
+
+// Add text node
+newHeading.appendChild(
+  document.createTextNode("New " + oldHeading.textContent)
+);
+
+// Get the parent element
+const parentOldHeading = oldHeading.parentElement;
+
+// replace
+parentOldHeading.replaceChild(newHeading, oldHeading);
+
+console.log(newHeading, oldHeading, parentOldHeading);
+
+//remove element
+
+const lis = document.querySelectorAll("li");
+const li = document.querySelector("li:nth-child(1)");
+const ul = document.querySelector("ul");
+
+// No genera error si intento removel el mismo elemento
+// remove list element
+li.remove();
+lis[0].remove();
+
+console.log(li, ul, lis);
+
+//remove child element
+ul.removeChild(lis[1]);
+
+// CLASSES and ATTR
+const firstLI = document.querySelector("li:first-child");
+
+const link = firstLI.children[0];
+
+// CLASSES
+
+let val;
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add("test");
+link.classList.remove("secondary-content");
+
+console.log("firstLi", link);
+console.log("classes", link.className);
+console.log(val);
+
+// ATTR
+
+val = link.getAttribute("href");
+link.setAttribute("test", "test");
+link.setAttribute("href", "https://www.google.com");
+val = link.hasAttribute("test");
+val = link.hasAttribute("title");
+link.removeAttribute("test");
+
+console.log("attributes", link);
+console.log(val);
