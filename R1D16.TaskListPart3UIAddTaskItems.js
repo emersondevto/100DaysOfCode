96524213a28d29a36ae2f27f791d8f1d064d4e4c
Emerson Volkov <emersonvolkov@gmail.com>
2019-09-26 07:10:05 -0500

    #100DaysOfCode R1D16 Creando mi primera aplicación con vanillajs, persistiendo las tareas del ToDo App en localStorage. #javascript. Aquí si debo contarles que no me gusto como lo resolvía el curso, así que lo hice a mi manera.

--- R1D16.TaskListPart3UIAddTaskItems.js
+++ R1D16.TaskListPart3UIAddTaskItems.js
@@ -7,19 +7,28 @@ const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
-// functionEvent add task
-function addTask(e) {
-  e.preventDefault();
-  if (taskInput.value === "") {
-    alert("Add a task");
+function getTasksFromLocalStorage() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
   }
+  return tasks;
+}
 
-  // create li element
+function storeTaskInLocalStorage(newTask) {
+  let tasks = getTasksFromLocalStorage();
+  tasks.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function buildTaskElement(tasks) {
   const li = document.createElement("li");
   // add a class
   li.className = "collection-item";
   // create the text node
-  li.appendChild(document.createTextNode(taskInput.value));
+  li.appendChild(document.createTextNode(tasks));
   // create new link element
   const link = document.createElement("a");
   // add the class
@@ -30,8 +39,47 @@ function addTask(e) {
   li.appendChild(link);
   // append link to ul
   taskList.appendChild(li);
+}
+
+// functionEvent add task
+function addTask(e) {
+  e.preventDefault();
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // // create li element
+  // const li = document.createElement("li");
+  // // add a class
+  // li.className = "collection-item";
+  // // create the text node
+  // li.appendChild(document.createTextNode(taskInput.value));
+  // // create new link element
+  // const link = document.createElement("a");
+  // // add the class
+  // link.className = "delete-item secondary-content";
+  // // add the icon
+  // link.innerHTML = '<i class="fa fa-remove"></i>';
+  // // append link to a
+  // li.appendChild(link);
+
+  buildTaskElement(taskInput.value);
+
+  // store on localstorage
+  storeTaskInLocalStorage(taskInput.value);
+
   //clear input
   taskInput.value = "";
+}
+
+//remove from local storage
+function removeTaskFromLocalStorage(taskElement) {
+  let tasks = getTasksFromLocalStorage();
+  var index = tasks.indexOf(taskElement.textContent);
+  if (index > -1) {
+    tasks.splice(index, 1);
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // delete element task
@@ -39,6 +87,7 @@ function deleteTask(e) {
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("Estas seguro?")) {
       e.target.parentElement.parentElement.remove();
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
 }
@@ -49,6 +98,7 @@ function clearTasks(e) {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+  localStorage.removeItem("tasks");
   // Es mas demorado con innerHTML
   // https://jsperf.com/innerhtml-vs-removechild/15
   // console.log(e.target);
@@ -69,8 +119,16 @@ function filterTasks(e) {
   // console.log(e.target.value);
 }
 
+// Get tasks from localstorage
+function loadTasks(e) {
+  let tasks = getTasksFromLocalStorage();
+  tasks.forEach(buildTaskElement);
+}
+
 // Cargar todos los eventListeners
 function loadEventListeners() {
+  // DOM Load Event
+  document.addEventListener("DOMContentLoaded", loadTasks);
   // add task event
   form.addEventListener("submit", addTask);
   // remove task event
@@ -83,5 +141,6 @@ function loadEventListeners() {
 
 // Cargar todos los eventListeners
 loadEventListeners();
+//
 
 // console.log(form, taskList, clearBtn, filter, taskInput);
