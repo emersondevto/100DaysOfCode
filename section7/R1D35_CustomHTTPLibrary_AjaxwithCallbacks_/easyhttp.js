09fabb1d99b5073ccfdb78896a8e7ac32832a80b
EmersonVolkov <emersonvolkov@gmail.com>
2019-10-17 03:18:57 +0000

    #100DaysOfCode bueno... lo siento... el dia me supero, mucho trabajo y termine molido, estudie como 30 minutos y ya el cuerpo me pide descansar.

--- section7/R1D35_CustomHTTPLibrary_AjaxwithCallbacks_/easyhttp.js
+++ section7/R1D35_CustomHTTPLibrary_AjaxwithCallbacks_/easyhttp.js
@@ -18,5 +18,19 @@ easyHTTP.prototype.get = function(url, callback) {
 };
 
 // Make an http post request
+
+easyHTTP.property.post = function (url,data,callback) {
+  this.http.open("POST", url, true)
+  
+  this.http.setRequestHeader('Content-type','application/json')
+  
+  let self = this;
+  this.http.onload = function() {
+      callback(null, self.http.responseText);
+  };
+  
+  this.http.send(JSON.stringify(data))
+}
+
 // Make an http put request
 // Make an http delete request
