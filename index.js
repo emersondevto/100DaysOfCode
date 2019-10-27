59ec776013b25a45931267c66f8d6e112e5972dc
Emerson <emersonvolkov@users.noreply.github.com>
2019-10-27 08:58:49 -0500

    Jugando con las aplicaciones para programar desde el iPhone. @playdotjs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('aHola mundo')
+//console.log('aHola mundo')}ñ
+
+async function holaMundo (){
+  return 'hola mundo 3'
+}
+
+console.log(holaMundo().then(res => console.log(res)))
+
+
+const hm = () => {
+  return 'hola mundo 2'
+}
+
+console.log(hm())
