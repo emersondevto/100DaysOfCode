145aa2e4e514797eeaf76b1663a9eeb8eceb0877
emersonvolkov <emersonvolkov@gmail.com>
2019-10-28 20:41:28 -0500

    #100DaysOfCode R1D48 Continuando con la construcción de la app Github Finder. Hoy se agrega look and feel de la carga de un perfil una vez se encuentra con la API de @github

---
+++ Libro_01_ModernJavaScriptFromTheBeginning/section7/R1D48_GithubFinderPart_3/app.js
@@ -0,0 +1,28 @@
+console.log("Github Finder");
+// Search input
+
+const searchUser = document.getElementById("searchUser");
+const gitHub = new GitHub();
+// Init UI
+const ui = new UI();
+
+// search input event listener
+searchUser.addEventListener("keyup", e => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    // make http call
+    gitHub.getUser(userText).then(data => {
+      if (data.status === 200) {
+        // Show el perfil
+        ui.showProfile(data.profile);
+      } else {
+        // show alert
+        console.log("El usuario no existe");
+      }
+    });
+  } else {
+    // clear the profile
+  }
+});
