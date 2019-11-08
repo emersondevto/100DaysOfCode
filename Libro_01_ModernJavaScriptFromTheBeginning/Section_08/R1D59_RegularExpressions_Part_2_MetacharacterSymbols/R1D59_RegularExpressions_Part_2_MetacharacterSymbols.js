2a5428dc5b6444e962cd63a0035178529e5d3d58
emersonvolkov <emersonvolkov@gmail.com>
2019-11-08 06:44:58 -0500

    #100DaysOfCode #JavaScript R1D59 Metacatracteres en RegEx
    \^
    $
    i
    .
    ?
    Hasta el momento nada complejo, solo es practicarlos para recordar que hace cada uno.

---
+++ Libro_01_ModernJavaScriptFromTheBeginning/Section_08/R1D59_RegularExpressions_Part_2_MetacharacterSymbols/R1D59_RegularExpressions_Part_2_MetacharacterSymbols.js
@@ -0,0 +1,39 @@
+console.log("R1D59_RegularExpressions_Part_2_MetacharacterSymbols");
+
+let re;
+// literal characters
+re = /hello/;
+re = /hello/i;
+
+// metacharacter symbols
+re = /^h/i; // ^ Must start with
+re = /^e/i; // ^ Must start with
+re = /d$/i; // $ Must ends with
+re = /world$/i; // $ Must ends with
+re = /^world$/i; // ^$ Must ends and start with
+re = /^Hello world$/i; // ^$ Must ends and start with
+re = /^.ello world$/; // . Matches any One Character
+// let str = "Hello world";
+re = /gre?a?y/i; // ? optional characters
+// let str = "grey";
+// let str = "gray";
+// let str = "griy";
+re = /gre?a?y\?/i; // \ Escape characters
+let str = "gray?";
+
+// String to match
+// let str = "Hello world";
+
+// Log results
+let resuls = re.exec(str);
+console.log(resuls);
+
+function reTest(re, str) {
+  if (re.test(str)) {
+    console.log(`${str} matched ${re.source}`);
+  } else {
+    console.log(`${str} does NOT matched ${re.source}`);
+  }
+}
+
+reTest(re, str);
