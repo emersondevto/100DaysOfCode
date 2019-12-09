344bc02b72be307e9ee15e5d7e56042c67aad6c7
emersonvolkov <emersonvolkov@gmail.com>
2019-12-08 20:35:53 -0500

    R1D88 Jugando con las formas de usar los export en los modules de ES2015. Haciendo uso de webpack y babel.

--- MOOC_01_ModernJavaScriptFromTheBeginning/Chapter_13_Microposts_Project_CRUD_Front_End_Webpack_Babel/babel_webpack_starter-master/src/app.js
+++ MOOC_01_ModernJavaScriptFromTheBeginning/Chapter_13_Microposts_Project_CRUD_Front_End_Webpack_Babel/babel_webpack_starter-master/src/app.js
@@ -1,10 +1,34 @@
-const greeting = "Hello World     ";
+// CommonJS Module Syntax
+const person = require("./mymodule1");
+
+console.log(person);
+console.log(person.name);
+
+// ES2015 Module
+import { person2, sayHello } from "./mymodule2";
+// import { sayHello } from "./mymodule2";
+
+console.log(person2);
+console.log(person2.name);
+console.log(sayHello());
+
+import * as mymodule2 from "./mymodule2";
+
+console.log(mymodule2.person2);
+console.log(mymodule2.person2.name);
+console.log(mymodule2.sayHello());
+
+import greeting from "./mymodule2";
+
 console.log(greeting);
 
-const getData = async url => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
+// const greeting = "Hello World     ";
+// console.log(greeting);
 
-getData("https://jsonplaceholder.typicode.com/posts");
+// const getData = async url => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+
+// getData("https://jsonplaceholder.typicode.com/posts");
---
+++ MOOC_01_ModernJavaScriptFromTheBeginning/Chapter_13_Microposts_Project_CRUD_Front_End_Webpack_Babel/babel_webpack_starter-master/src/mymodule2.js
@@ -0,0 +1,12 @@
+export const person2 = {
+  name: "Jhon",
+  age: 30
+};
+
+export function sayHello() {
+  return `Hello ${person2.name}`;
+}
+
+const greeting = "Hello World";
+
+export default greeting;
