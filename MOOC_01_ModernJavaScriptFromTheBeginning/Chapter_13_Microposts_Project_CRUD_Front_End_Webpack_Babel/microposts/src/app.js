f3d4c3620ed1522dabb55ac6af41e95a622b8815
emersonvolkov <emersonvolkov@gmail.com>
2019-12-09 20:46:08 -0500

    R1D89 Se acuerdan de ese proyecto easyHttp que creamos para aprender a usan async/await... bueno, lo empezamos usndo  en este proyecto de miniposts.

---
+++ MOOC_01_ModernJavaScriptFromTheBeginning/Chapter_13_Microposts_Project_CRUD_Front_End_Webpack_Babel/microposts/src/app.js
@@ -0,0 +1,12 @@
+import { http } from "./http";
+import { ui } from "./UI";
+
+// Get Posts on Dom Loads
+document.addEventListener("DOMContentLoaded", getPosts);
+
+function getPosts() {
+  http
+    .get("http://localhost:3000/posts")
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
