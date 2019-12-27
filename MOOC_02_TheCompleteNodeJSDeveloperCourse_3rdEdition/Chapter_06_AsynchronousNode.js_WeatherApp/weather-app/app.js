e0aebeef761c69458e191c06fa565bf3fd259f84
emersonvolkov <emersonvolkov@gmail.com>
2019-12-26 20:37:17 -0500

    R2D05 Estoy aprendiendo como NO hacer un curso de nodejs. Siempre se aprende algo...

--- MOOC_02_TheCompleteNodeJSDeveloperCourse_3rdEdition/Chapter_06_AsynchronousNode.js_WeatherApp/weather-app/app.js
+++ MOOC_02_TheCompleteNodeJSDeveloperCourse_3rdEdition/Chapter_06_AsynchronousNode.js_WeatherApp/weather-app/app.js
@@ -1,9 +1,13 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/f6c77d678ec62a2b67eb9b8b6817041d/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/f30ab4a3eee347a9515781ee47d8feff/37.8267,-122.4233?lang=es&units=si";
 
-request({ url }, (error, response) => {
-  const data = response.body;
-  console.log(data);
+request({ url, json: true }, (error, response) => {
+  // Con la opcion json no es necesario hacer JSON.parse
+  const data = response.body.currently;
+  // console.log(data);
+  console.log(
+    `${response.body.daily.data[0].summary} En este momento hay ${data.temperature} grados afuera. Hay una probabilidad de ${data.precipProbability}% de llover.`
+  );
 });
