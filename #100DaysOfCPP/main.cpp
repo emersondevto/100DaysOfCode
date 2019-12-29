b5dcffbd550f2afb42f77dd593fd543521b7729f
emersonvolkov <emersonvolkov@gmail.com>
2019-12-28 20:53:55 -0500

    R2D07 Estoy aprendiendo tanto C++ como Javascript así que voy a ir publicando mi progreso de ambos lenguajes.

---
+++ #100DaysOfCPP/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  #100DaysOfCPP
+//
+//  Created by Emerson on 28/12/19.
+//  Copyright © 2019 Emerson. All rights reserved.
+//
+
+#include <iostream>
+//using std::cout;
+//using std::endl;
+
+using namespace std;
+
+void hola (){
+    cout << "hola func";
+}
+
+int main(int argc, const char * argv[]) {
+    hola();
+    // insert code here...
+    cout << "Hello, World!\n";
+    std::cout << __cplusplus;
+    return 0;
+}
